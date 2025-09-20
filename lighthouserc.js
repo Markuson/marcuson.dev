074fe015d3945e3014a54be291ae28a6ff107b1e
Marc Usón <el.marcuson@gmail.com>
2025-09-21 00:25:12 +0200

    chore: fix lighthouse config and ignore artifacts

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,4 +1,4 @@
-module.exports = {
+const config = {
   ci: {
     collect: {
       // Build the site before running Lighthouse
@@ -54,3 +54,5 @@ module.exports = {
     }
   }
 };
+
+export default config;
